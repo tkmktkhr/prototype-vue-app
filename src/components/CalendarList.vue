15297d0f299dd37f5d204483da1bb56561ca74cd
Takahiro Takamuku <mukku.wordpress@gmail.com>
2019-08-08 09:31:32 -0400

    Create first app

---
+++ src/components/CalendarList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="calendar-list">
+    calendar-list
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'calendar-list'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="hello">
+     <h1>{{ msg }}</h1>
+     <h2>Essential Links</h2>
+   </div>
+   <div id="calendar-list">
+     <calendar-list></calendar-list>
+     <!-- </calendar-list> -->
+   </div>
+ </div>
+</template>
+
+<script>
+import CalendarList from './CalendarList'
+
+export default {
+  name: 'HelloWorld',
+
+  components: {
+    CalendarList
+  },
+  data () {
+    return {
+
+      msg: 'Welcome to prototyape-app'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
